cad789ff80e13a41bfc6710997dc9a8a3b80c6d2
alexisartus96 <alexis.artus@fing.edu.uy>
2019-06-15 22:29:44 -0300

    merge

--- web/src/main/java/constantes/Constantes.java
+++ web/src/main/java/constantes/Constantes.java
@@ -1,7 +1,7 @@
 package constantes;
 
 public class Constantes {
-	public static final String host = "http://192.168.43.234:8080";
+	public static final String host = "http://localhost:8080";
 	public static final String processKey = "process";
 	public static final String user = "admin";
 	public static final String password = "test";
